71a92fbedec852a1993b5e19f0d66981553204fd
sairoutine <sairoutine@gmail.com>
2017-04-05 14:35:26 +0900

    update: broadcast

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ var WebSocket = require('ws');
 
 var wss = new WebSocket.Server({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
-	ws.on('message', function incoming(message) {
-		console.log('received: %s', message);
+/*
+// Broadcast to all.
+wss.broadcast = function broadcast(data) {
+	wss.clients.forEach(function each(client) {
+		if (client.readyState === WebSocket.OPEN) {
+			client.send(data);
+		}
 	});
+};
+*/
 
-	ws.send('something');
+wss.on('connection', function connection(ws) {
+	ws.on('message', function incoming(data) {
+		// Broadcast to everyone else.
+		wss.clients.forEach(function each(client) {
+			if (client !== ws && client.readyState === WebSocket.OPEN) {
+				client.send(data);
+			}
+		});
+	});
 });
