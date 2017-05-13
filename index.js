d7f90b22fd4a43d7cdd82e3bf250efafffc51340
sairoutine <sairoutine@gmail.com>
2017-05-13 23:25:20 +0900

    ing

--- index.js
+++ index.js
@@ -14,13 +14,54 @@ wss.broadcast = function broadcast(data) {
 };
 */
 
+// a event which one of the client connects me
 wss.on('connection', function connection(ws) {
-	ws.on('message', function incoming(data) {
+	// a event which the client sends me
+	ws.on('message', function (data) {
+
+		// TODO: parse data
+		// var parsed_data = parse_data(data);
+
+		//if (!parsed_data.event) return;
+
+		// TODO: create event name to function
+		//var func = EVENT_NAME_TO_FUNCION[ parsed_data.event ];
+
+		//func(ws, parsed_data.arguments);
+
+		/*
 		// Broadcast to everyone else.
 		wss.clients.forEach(function each(client) {
 			if (client !== ws && client.readyState === WebSocket.OPEN) {
 				client.send(data);
 			}
 		});
+		*/
 	});
 });
+
+function parse_data () {
+
+}
+
+function subscribe () {
+
+}
+
+function publish () {
+
+}
+
+function create_room () {
+
+}
+
+function join () {
+//-> ROOM_CLIENTS_LIMIT
+
+}
+
+function send_data () {
+//-> ROOM_CLIENTS_LIMIT
+
+}
