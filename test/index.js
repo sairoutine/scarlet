75180db34d50759dcd3ad1f0bc267e9f197ab000
sairoutine <sairoutine@gmail.com>
2017-06-08 20:40:25 +0900

    update: test

--- test/index.js
+++ test/index.js
@@ -22,8 +22,14 @@ describe('Scarlet', function(){
 	beforeEach(function(done){
 		ws1 = new WebSocket('ws://localhost:8080/');
 		ws1.on('open', function () {
-			done();
+
+			ws2 = new WebSocket('ws://localhost:8080/');
+			ws2.on('open', function () {
+				done();
+			});
+
 		});
+
 	});
 
 	afterEach(function(done) {
@@ -47,5 +53,27 @@ describe('Scarlet', function(){
 		});
 		ws1.send("C2S_PING");
 	});
+
+	it('should be enable to subscribe/publish', function(done){
+		var channel_name = "test";
+		var message      = "test";
+		ws1.on('message', function (data) {
+			if (data.match(/^S2C_PUBLISHED/)) {
+				done();
+			}
+		});
+
+		ws1.send("C2S_SUBSCRIBE " + channel_name);
+
+		ws2.send("C2S_PUBLISH " + channel_name + " " + message);
+	});
+
+	/* TODO:
+	c2s_create_room:   create_room,
+	c2s_join:          join,
+	c2s_get_room_list: get_room_list,
+	c2s_send_data:     send_data,
+	c2s_join_random:   join_random,
+	*/
 
 });
